702ff190a1b0759f0782b999920a82a36b263acc
Lavanya Koppula <koppulalavanya35@gmail.com>
2025-05-18 13:08:55 +0530

    Done Changes

--- iboxes.js
+++ iboxes.js
@@ -1,9 +1,12 @@
 document.querySelectorAll('.box').forEach(box => {
     box.addEventListener('click', () => {
+        // Deselect all
         document.querySelectorAll('.box').forEach(b => {
             b.classList.remove('active');
             b.querySelector('input[type="radio"]').checked = false;
         });
+
+        // Select current
         box.classList.add('active');
         box.querySelector('input[type="radio"]').checked = true;
     });
